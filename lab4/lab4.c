25635c2b113a3b9056db901802e7b1eb551b4b9e
Long Le <longle1@illinois.edu>
2016-03-01 16:40:07 -0600

    lab4 final version

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -8,8 +8,8 @@
 #include "linuxanalog.h"
 
 timer_t timer1;
-uint16_t posVol = (uint16_t) (2.0+5.0)/10.0*4096;
-uint16_t negVol = (uint16_t) (-2.0+5.0)/10.0*4096;
+uint16_t posVol; //= (uint16_t) (2.0+5.0)/10.0*4096;
+uint16_t negVol; //= (uint16_t) (-2.0+5.0)/10.0*4096;
 int state = 0;
 
 void setup_timer();
@@ -17,6 +17,21 @@ void handler_function(int signum);
 void setup_signal_handler();
 
 int main(){
+	int user_voltage_1 = 6;
+	int user_voltage_2 = 6;
+	printf("Enter voltage #1 (-5 to +5 volts, other to quit):");
+	while (user_voltage_1 > 5 || user_voltage_1 < -5){	
+		//printf("Enter voltage #1 (-5 to +5 volts, other to quit):");
+		scanf("%d", &user_voltage_1);
+	}
+	printf("Enter voltage #2 (-5 to +5 volts, other to quit):");
+	while ((user_voltage_2 < -5 || user_voltage_2 > 5)){// || user_voltage_1 < user_voltage_2){
+		//printf("Enter voltage #2 (-5 to +5 volts, other to quit):");
+		scanf("%d", &user_voltage_2); 
+	}
+	 posVol = (uint16_t) (user_voltage_1+5.0)/10.0*4096;
+	 negVol = (uint16_t) (user_voltage_2+5.0)/10.0*4096;
+
 	printf("Init... \n");
 	das1602_initialize();
 	printf("DAS 1602 Initted\n");
@@ -40,6 +55,7 @@ int main(){
 }
 
 void setup_timer(){
+	uint16_t user_frequency = -1;
 	if (timer_create(CLOCK_REALTIME,NULL,&timer1) != 0){
 		// If there is an error, print out a message and exit.
 		perror("timer_create");
@@ -61,7 +77,16 @@ void setup_timer(){
 	timer1_time.it_value.tv_sec = 0; // 2 seconds
 	timer1_time.it_value.tv_nsec = 500000000; // 0.5 seconds (5e8 nanoseconds)
 	timer1_time.it_interval.tv_sec = 0; // 0 seconds
-	timer1_time.it_interval.tv_nsec = 5000000; // 10 milliseconds 
+
+	printf("Enter frequency (0 to 500000000.000000 Hz, other to quit):");
+	scanf("%d", &user_frequency);
+	while (user_frequency > 500000000 || user_frequency < 0){
+		printf("Enter frequency (0 to 500000000.000000 Hz, other to quit):");
+		scanf("%d", &user_frequency);
+	}
+
+	timer1_time.it_interval.tv_nsec = 500000000 / user_frequency;// *(1/2000000000);//* 50000;//5000000; // 10 milliseconds 
+
 	// Schedule the timer.
 	if (timer_settime(timer1, 0, &timer1_time, NULL) != 0){
 		 //If there is an error, print out a message and exit.
@@ -73,7 +98,7 @@ void setup_timer(){
 // Define a signal handler function. The function name can be anything, but
 // it must accept a single integer.
 void handler_function(int signum){
-	printf("SIGALARM catched.\n");
+	//printf("SIGALARM catched.\n");
 	
 	state = !state;
 	if (state){
